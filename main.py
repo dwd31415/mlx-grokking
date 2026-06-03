b9a443a7682a181348c8e0e4ac2b8d79dec7e485
Adrian Dawid <adriandwd@gmail.com>
2026-06-03 23:31:02 +0100

    Change prime

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from data import grokking_data
 
 parser = argparse.ArgumentParser(add_help=True)
 # data args
-parser.add_argument('--p', type=int, default=97, help='prime number')
+parser.add_argument('--p', type=int, default=71, help='prime number')
 parser.add_argument('--op', type=str, default='/',
                     help='operation', choices=['*', '/', '+', '-'])
 parser.add_argument('--train-fraction', type=float,
@@ -35,7 +35,7 @@ parser.add_argument('--warmup', type=int, default=10, help='warmup steps')
 parser.add_argument('-b', '--batch_size', type=int,
                     default=512, help='batch size')
 parser.add_argument('-e', '--epochs', type=int,
-                    default=150, help='number of epochs')
+                    default=300, help='number of epochs')
 # misc args
 parser.add_argument('--seed', type=int, default=random.randint(0,1000), help='random seed')
 parser.add_argument('--cpu', action='store_true', help='use cpu only')
